381107a98b1bfbbdc5e900b4e0a4437652787dc1
Angristan <angristan@pm.me>
2018-07-28 18:30:02 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
+
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({ todos });
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  } else {
+    Todo.findById(id).then((todo) => {
+      if (!todo) {
+        res.status(404).send();
+      } else {
+        res.send({ todo });
+      }
+    }).catch((err) => {
+      res.status(400).send();
+    });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
+
+module.exports = { app };
